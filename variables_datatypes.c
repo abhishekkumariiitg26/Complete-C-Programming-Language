f66f99ec2b3877fca9819b55d5b9c5e5bd4dfd67
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-20 18:56:02 +0530

    inital commit

---
+++ variables_datatypes.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x;
+    x=15;
+    printf("%d\n",x);
+    //datatypes
+    int a=20;
+    float b=2.2154;
+    double c=2.215487963;
+    char d ='A';
+    printf("%d %f %c",a,b,d);
+    return 0;
+}
