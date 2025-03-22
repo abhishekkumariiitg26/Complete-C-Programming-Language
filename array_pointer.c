d0e33a57a263b91c526a18669c8682168257cf04
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-22 08:55:50 +0530

    initial commit

---
+++ array_pointer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int *ptr=arr;
+    printf("%d\n",arr);
+    printf("%d\n",ptr);
+    printf("%d\n",*(ptr));
+    printf("%d\n",*(ptr+1));
+    printf("%d\n",*(ptr+2));
+    printf("%d\n",*(ptr+3));
+    printf("%d\n",*(ptr+4));
+    printf("%d\n",*(ptr+11)); // garbage value
+    return 0;
+}
