a6a1bebe093425d4928f7dc9d365870ece436253
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-22 08:15:11 +0530

    initial commit

--- pointers.c
+++ pointers.c
@@ -2,9 +2,14 @@
 int main(){
     int a=11;
     int *ptr=&a;
+    int *ptr_null=NULL;
     printf("%d\n",a);
     printf("%d\n",ptr);
     printf("%d\n",&a);
     printf("%d\n",ptr);
+    printf("%d\n",&ptr); // gives address of the pointer
+    printf("%d\n",ptr_null);
+    printf("%d\n",*ptr_null);
+
     return 0;
 }
