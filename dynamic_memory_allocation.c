5480dcb61581b9a435f02f877d5f67ee0c8e3cb1
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-03-23 09:44:14 +0530

    calloc realloc free added

--- dynamic_memory_allocation.c
+++ dynamic_memory_allocation.c
@@ -1,26 +1,46 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
     int n;
     printf("Enter the number of element you want in the dynamic array: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     int *ptr;
-    ptr=(int*)malloc(n*sizeof(int));
+    // ptr=(int*)malloc(n*sizeof(int));
+    ptr = (int *)calloc(n, sizeof(int)); // this line for calloc otherwise both are same
+    // but calloc initialise with 0;
 
     for (int i = 0; i < n; i++)
     {
-        printf("\nEnter the value of %d element: ",i+1);
-        scanf("%d",&ptr[i]);
+        printf("\nEnter the value of %d element: ", i + 1);
+        scanf("%d", &ptr[i]);
     }
     printf("The elements of the array are: ");
     for (int i = 0; i < n; i++)
     {
-        printf("%d ",ptr[i]);
-       
+        printf("%d ", ptr[i]);
     }
+    // realloc
+    int p;
+    printf("\nEnter the new number of element you want in the new dynamic array: ");
+    scanf("%d", &p);
 
     
+    // ptr=(int*)malloc(n*sizeof(int));
+    ptr = (int *)realloc(ptr, p*sizeof(int)); // this line for calloc otherwise both are same
+    // but calloc initialise with 0;
 
+    for (int i = 0; i < p; i++)
+    {
+        printf("\nEnter the new value of %d element: ", i + 1);
+        scanf("%d", &ptr[i]);
+    }
+    printf("The elements of the new array are: ");
+    for (int i = 0; i < p; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+    free(ptr);
     return 0;
 }
